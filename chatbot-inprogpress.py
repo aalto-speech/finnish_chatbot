7ceb33318b41f49c2143b239841350b31138b19c
juholeinonen <juho.leinonen@aalto.fi>
2020-04-20 13:02:26 +0300

    deleted extra functions

--- chatbot-inprogpress.py
+++ chatbot-inprogpress.py
@@ -63,39 +63,6 @@ printLines(os.path.join(corpus, "10k_suomi24_morfs.txt"))
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# Splits each line of the file into a dictionary of fields
-def loadLines(fileName, fields):
-    lines = {}
-    with open(fileName, 'r', encoding='iso-8859-1') as f:
-        for line in f:
-            values = line.split(" +++$+++ ")
-            # Extract fields
-            lineObj = {}
-            for i, field in enumerate(fields):
-                lineObj[field] = values[i]
-            lines[lineObj['lineID']] = lineObj
-    return lines
-
-
-# Groups fields of lines from `loadLines` into conversations based on *movie_conversations.txt*
-def loadConversations(fileName, lines, fields):
-    conversations = []
-    with open(fileName, 'r', encoding='iso-8859-1') as f:
-        for line in f:
-            values = line.split(" +++$+++ ")
-            # Extract fields
-            convObj = {}
-            for i, field in enumerate(fields):
-                convObj[field] = values[i]
-            # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
-            utterance_id_pattern = re.compile('L[0-9]+')
-            lineIds = utterance_id_pattern.findall(convObj["utteranceIDs"])
-            # Reassemble lines
-            convObj["lines"] = []
-            for lineId in lineIds:
-                convObj["lines"].append(lines[lineId])
-            conversations.append(convObj)
-    return conversations
 
 
 # Extracts pairs of sentences from conversations
@@ -120,18 +87,10 @@ delimiter = '\t'
 # Unescape the delimiter
 delimiter = str(codecs.decode(delimiter, "unicode_escape"))
 
-# Initialize lines dict, conversations list, and field ids
-lines = {}
-conversations = []
-MOVIE_LINES_FIELDS = ["lineID", "characterID", "movieID", "character", "text"]
-MOVIE_CONVERSATIONS_FIELDS = ["character1ID", "character2ID", "movieID", "utteranceIDs"]
 
 # Load lines and process conversations
 print("\nProcessing corpus...")
 #lines = loadLines(os.path.join(corpus, "movie_lines.txt"), MOVIE_LINES_FIELDS)
-print("\nLoading conversations...")
-#conversations = loadConversations(os.path.join(corpus, "movie_conversations.txt"),
-#                                  lines, MOVIE_CONVERSATIONS_FIELDS)
 
 # Write new csv file
 print("\nWriting newly formatted file...")
